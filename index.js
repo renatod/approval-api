4f1fbf464a1b1fc879a8338a083acbd4ce5bcb32
renatod <renato.dias@me.com.br>
2020-06-01 13:51:53 -0300

    dummy

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser())
+
+app.get('/:processType/:processNumber/actions', (req, res) => {
+  const { processType, processNumber } = req.params
+  res.json([{
+    label: 'Approve',
+    action: `/${processType}/${processNumber}/action/approve`
+  }, {
+    label: 'Refuse',
+    action: `/${processType}/${processNumber}/action/resuse`,
+    form: [{
+      input: 'textarea',
+      label: 'Reason',
+      required: true
+    }]
+  }, {
+    label: 'Delegate',
+    url: 'https://www.google.com.br/'
+  }])
+});
+
+app.post('/:processType/:processNumber/action/:action', (req, res) => {
+  const { processNumber, action } = req.params
+  res.json({
+    message: `Process ${processNumber} successfully ${action}`,
+    form: JSON.stringify(req.body)
+  })
+})
+
+app.listen(process.env.PORT || 3100, () => {
+  console.log('API running!')
+});
